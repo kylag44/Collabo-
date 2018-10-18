a777995ac8689bba9c765a64079f4bf82dd2bcda
Kyla <kylag44@gmail.com>
2018-10-18 14:52:33 -0400

    finished user cell subview layouts programmatically

--- Collabo/Collabo/View/Cells.swift
+++ Collabo/Collabo/View/Cells.swift
@@ -29,17 +29,65 @@ class UserCell: DatasourceCell {
     }
   }
   
+  ///for the profile image view
+  let profileImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.backgroundColor = .white
+    return imageView
+  }()
+  
   let nameLabel: UILabel =  {
     let label = UILabel()
     label.text = "TEST TEST TEST"
+    label.backgroundColor = .darkGray
+    label.textColor = .white
+    label.font = UIFont(name: "AvenirNext-Bold", size: 20)
     return label
+  }()
+  
+  let userNameLabel: UILabel =  {
+    let label = UILabel()
+    label.text = "User Name"
+    label.backgroundColor = .darkGray
+    label.textColor = .white
+    label.font = UIFont(name: "AvenirNext", size: 20)
+    return label
+  }()
+  
+  let bioTextView: UITextView = {
+    let textView = UITextView()
+    textView.backgroundColor = .gray
+    textView.textColor = .white
+    textView.font = UIFont(name: "AvenirNext", size: 20)
+    return textView
+  }()
+  
+  let followButton: UIButton = {
+    let button = UIButton()
+    button.backgroundColor = .white
+    return button
   }()
   
   override func setupViews() {
     super.setupViews()
     backgroundColor = .cyan
+    
+    addSubview(profileImageView)
     addSubview(nameLabel)
-    nameLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+    addSubview(userNameLabel)
+    addSubview(bioTextView)
+    addSubview(followButton)
+    
+    profileImageView.anchor(self.topAnchor, left: self.leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
+    
+    nameLabel.anchor(profileImageView.topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: followButton.leftAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 20)
+    
+    userNameLabel.anchor(nameLabel.bottomAnchor, left: nameLabel.leftAnchor, bottom: nil, right: nameLabel.rightAnchor, topConstant: 8, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 20)
+
+    bioTextView.anchor(userNameLabel.bottomAnchor, left: userNameLabel.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+    
+    followButton.anchor(topAnchor, left: nil, bottom: nil, right: self.rightAnchor, topConstant: 12, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 120, heightConstant: 34)
   }
+  
 }
 
